b2b30b8a1832339ae2e0570f3876d59196efc0c7
윤현지 <hyoon@student.42seoul.kr>
2021-06-17 18:41:46 +0900

    update

---
+++ Session.hpp
@@ -0,0 +1,16 @@
+#ifndef FT_SESSION_HPP
+# define FT_SESSION_HPP
+
+#include <string>
+#include <sys/select.h>
+#include "Server.hpp"
+#include "Fd.hpp"
+#include <sys/resource.h>
+
+class Session
+{
+	private:
+	public:
+};
+
+#endif
