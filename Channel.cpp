579010500f6172c5e2449442b0f3a2d7a4c0447b
jikwon <jikwon@student.42seoul.kr>
2021-06-19 16:24:06 +0900

    channel

--- Channel.cpp
+++ Channel.cpp
@@ -1,6 +1,66 @@
 #include "include/Channel.hpp"
 
+Channel::Channel(User *creator, std::string const& name, std::string const& topic)
+	: sName(name), sTopic(topic)
+{
+	mUsers[creator->nick()] = creator;
+	mOperators[creator->nick()] = creator;
+}
+
+Channel::~Channel()
+{
+	//write
+}
+
+void		Channel::addUser(User *user)
+{
+	//write
+}
+
+void		Channel::removeUser(User *user)
+{
+	//write
+}
+
+bool		Channel::hasUser(User *user)
+{
+	//write
+	return (true);
+}
+
+bool		Channel::isOperator(User *user)
+{
+	//write
+	return (true);
+}
+
+void		Channel::broadcast(std::string const& message)
+{
+	//write
+}
+
+std::string	Channel::password() const
+{
+	return (sPassword);
+}
+
+std::string Channel::topic() const
+{
+	return (sTopic);
+}
+
 std::string		Channel::name() const
 {
 	return (sName);
+}
+
+int				Channel::userCount() const
+{
+	return (mUsers.size());
+}
+
+bool			Channel::hasPass() const
+{
+	//write
+	return (false);
 }
--- include/Channel.hpp
+++ include/Channel.hpp
@@ -17,7 +17,23 @@ class Channel
 		std::string			sPassword;
 		bool				bPassword;
 		Usermap 			mUsers;
+		Usermap				mOperators;
 	public:
+		Channel(User *creator, std::string const& name, std::string const& topic = "");
+		~Channel();
+		/*
+		void	cmdTopic(std::string const& topic);
+
+		*/
+		void	addUser(User *user);
+		void	removeUser(User *user);
+		bool	hasUser(User *user);
+		bool	isOperator(User *user);
+		void	broadcast(std::string const& message);
+		std::string password() const;
+		std::string topic() const;
 		std::string name() const;
+		int		userCount() const;
+		bool	hasPass() const;
 };
 #endif
